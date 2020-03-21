935f31f14bbb5503cb11627b33cbc0d28653537a
npiacen <47162082+npiacen@users.noreply.github.com>
2020-03-20 21:38:09 -0500

    confirmed and deaths plots added

--- covidPandasDemo.py
+++ covidPandasDemo.py
@@ -9,4 +9,18 @@ confirmed = pd.read_csv('time_series_19-covid-Confirmed.csv')
 deaths = pd.read_csv('time_series_19-covid-Deaths.csv')
 recovered = pd.read_csv('time_series_19-covid-Recovered.csv')
 
-plot = deaths.plot(kind='bar',x='Country/Region',y='3/16/20')
+usDeaths = deaths['Country/Region'] == 'US'
+showDeaths = deaths[usDeaths][['Province/State', '3/20/20']]
+sortedDeaths = showDeaths.sort_values( by='3/20/20', ascending=False)
+top50Deaths = sortedDeaths[:50].plot(kind='bar',x='Province/State',y='3/20/20')
+
+usconfirmed = confirmed['Country/Region'] == 'US'
+showconfirmed = confirmed[usconfirmed][['Province/State', '3/20/20']]
+sortedconfirmed = showconfirmed.sort_values( by='3/20/20', ascending=False)
+top50confirmed = sortedconfirmed[:50].plot(kind='bar',x='Province/State',y='3/20/20')
+
+#usRecovered = recovered['Country/Region'] == 'US'
+##hasComma = ',' in deaths['Province/State']
+#showRecovered = recovered[usRecovered][['Province/State', '3/20/20']]
+#
+#plot = showDeaths.plot(kind='bar',x='Province/State',y='3/20/20')
