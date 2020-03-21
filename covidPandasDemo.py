a16d0f7f4eb7d0400f54d1b688bdfc2c78c93364
npiacen <47162082+npiacen@users.noreply.github.com>
2020-03-21 12:32:11 -0500

    updated davids code to pull straight from COVID database

--- covidPandasDemo.py
+++ covidPandasDemo.py
@@ -1,13 +1,21 @@
 # Pandas Demo
 # For Pandas Lightning Talk
-# Copyright Nick Piacente
+# Copyright Nick Piacente, David Cayll, Ziam Ghaznavi
 
 import pandas as pd
 import matplotlib.pyplot as plt
 
-confirmed = pd.read_csv('time_series_19-covid-Confirmed.csv')
-deaths = pd.read_csv('time_series_19-covid-Deaths.csv')
-recovered = pd.read_csv('time_series_19-covid-Recovered.csv')
+#confirmed = pd.read_csv('time_series_19-covid-Confirmed.csv')
+#deaths = pd.read_csv('time_series_19-covid-Deaths.csv')
+#recovered = pd.read_csv('time_series_19-covid-Recovered.csv')
+
+deaths_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv'
+confirmed_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
+recovered_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv'
+
+deaths = pd.read_csv(deaths_url)
+confirmed = pd.read_csv(confirmed_url)
+recovered = pd.read_csv(recovered_url)
 
 usDeaths = deaths['Country/Region'] == 'US'
 showDeaths = deaths[usDeaths][['Province/State', '3/20/20']]
