cdf96eafa0d261dd4ff2cb6737447d2784c3ad24
dcayll <60239356+dcayll@users.noreply.github.com>
2020-03-21 11:17:49 -0500

    David update to pulling csv

---
+++ david_update.py
@@ -0,0 +1,10 @@
+# David Cayll
+# pandas lightning talk
+
+deaths_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv'
+confirmed_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
+recovered_url = 'https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv'
+
+updated_deaths = pd.read_csv(deaths_url, index_col=0)
+updated_confirmed = pd.read_csv(deaths_url, index_col=0)
+updated_recovered = pd.read_csv(deaths_url, index_col=0)
